38e6adf4ab0e2847bd6478bfac8d46fc26773803
Aniket Kumar Singh <aniketsingh7400@gmail.com>
2021-07-19 19:47:08 +0530

    Second Commit

--- DataTypes/Opposite number.js
+++ DataTypes/Opposite number.js
@@ -1,6 +1,13 @@
+// Very simple, given a number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+
 function opposite(number) {
     return number - 2 * number;
 }
-
-var num = 14;
-console.log(opposite(num));
--- DataTypes/Printing Array elements with Comma delimiters.js
+++ DataTypes/Printing Array elements with Comma delimiters.js
@@ -1,7 +1,14 @@
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+
+
 function printArray(array) {
     array = array.map(String);
     return array.join(",");
 }
-
-var data = [2, 4, 5, 2];
-console.log(printArray(data));
